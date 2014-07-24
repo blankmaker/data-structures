e7e201f5a68c1f81395087f23b2ee65e481de96c
David <davidnguyen84@gmail.com>
2014-07-24 15:38:48 -0700

    Completed functionality for first three parts of Sprint 1. Completed for both stack and queue. Delay in commits due to internet outage.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,37 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+  someInstance.sizeOfQueue = 0;
+  someInstance.curPos = 0;
+  someInstance.nextPos = 0;
+
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
+  queueMethods.enqueue = function(value){
+    this.sizeOfQueue++;
+    this.storage[this.nextPos] = value;
+    this.nextPos++;
+  };
 
+  queueMethods.dequeue = function(){
+    if (this.sizeOfQueue > 0) {
+      var result = this.storage[this.curPos];
+      delete this.storage[this.curPos];
+      this.sizeOfQueue--;
+      this.curPos++;
+      return result;
+    }
 
+  };
+
+  queueMethods.size = function(){
+    return this.sizeOfQueue;
+  };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.sizeOfStack = 0;
+
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+};
+
+  stackMethods.push = function(value){
+    this.storage[this.sizeOfStack] = value;
+    this.sizeOfStack++;
+  };
+
+  stackMethods.pop = function(){
+    if (this.sizeOfStack > 0) {
+      this.sizeOfStack--;
+      var result = this.storage[this.sizeOfStack];
+      delete this.storage[this.sizeOfStack];
+      return result;
+    }
+  };
+
+  stackMethods.size = function(){
+    return this.sizeOfStack;
+  };
 
