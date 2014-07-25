05ac08f0a3607688f04dd9071465cf9f3aa083e6
David <davidnguyen84@gmail.com>
2014-07-25 16:08:48 -0700

    Completed hash table Uncommented specRunner for binTree, and added log to spec. Started binTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,51 @@
 var makeBinarySearchTree = function(value){
+  // make a binTree object
+  var binTree = {};
+
+  // create binTree.left and right, set to null by default
+  binTree.left = null;
+  binTree.right = null;
+
+  // create binTree.value = value
+  binTree.value = value;
+
+  // extend bintreemethods object to here
+  _.extend(binTree, binTreeMethods);
+
+  // return binTree
+  return binTree;
+};
+
+// make a binTreeMethods object
+var binTreeMethods = {};
+
+// create insert method
+binTreeMethods.insert = function(value) {
+  var newBinTree = makeBinarySearchTree(value);
+
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = newBinTree;
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = newBinTree;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+// create contains
+binTreeMethods.contains = function(target) {
+
+};
+
+// create depthFirstLog
+binTreeMethods.depthFirstLog = function(callback) {
+
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,25 +7,27 @@ HashTable.prototype.insert = function(k, v){ // 'cat', 'meow'
   var i = getIndexBelowMaxForKey(k, this._limit); // i = hashed key ('cat', 8)
   // if the index 'i' is undefined, create a new array at i
   if (this._storage.get(i) === undefined) {
-    this._storage.set(i, v);
+    this._storage.set(i, []);
   }
-
-  // this._storage[i].push('Seagal');
+  var newObj = {};
+  newObj[k] = v;
+  this._storage.get(i).push(newObj);
 
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   // if i is undefined, return false;
-  if (this._storage[i] === undefined) {
-    return false;
+  if (this._storage.get(i) === undefined) {
+    return null;
   }
-  this._storage[i].each(function(value, key, collection) {
-    if (value.k !== undefined) {
-      return value.k;
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+     if (bucket[j][k] !== undefined) {
+      return bucket[j][k];
     }
-  });
-  return false;
+  }
+  return null;
   // if i exists, loop through i bucket
   //
     // if k if found, return the value, else
@@ -34,6 +36,18 @@ HashTable.prototype.retrieve = function(k){
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(i) === undefined) {
+    return null;
+  }
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+     if(bucket[j][k] !== undefined) {
+      bucket.splice(j,1);
+    }
+  }
+  return null;
+
 
 };
 /*
