c780e6b2c819e9d2618e5ca4794e7d21358500c9
David <davidnguyen84@gmail.com>
2014-07-26 10:32:08 -0700

    Completed 'graph', but we should rework to use arrays instead of objects

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -24,13 +24,13 @@ Graph.prototype.addNode = function(newNode, toNode){
   // if this isn't the first node AND no 'toNode' is provided
   if (nodeToAdd !== this._firstNode && toNode === undefined) {
     // set 'toNode' to be the first node
-    toNode = this._firstNode;
+    toNode = this._firstNode.value;
   }
 
   this._storage[newNode] = nodeToAdd;
 
   if (toNode !== undefined) {
-    this.addEdge(newNode, toNode.value);
+    this.addEdge(newNode, toNode);
   }
 };
 
@@ -39,6 +39,15 @@ Graph.prototype.contains = function(node){
 };
 
 Graph.prototype.removeNode = function(node){
+
+  // loop through this._storage and
+  for (var key in this._storage[node].edges) {
+    this.removeEdge(node, key);
+  }
+  delete this._storage[node];
+    // set temp variable to 'toNode'
+    // call this._storage[node].removeEdge(node, temp )
+  // delete node
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
@@ -53,6 +62,12 @@ Graph.prototype.addEdge = function(fromNode, toNode){
 Graph.prototype.removeEdge = function(fromNode, toNode){
   delete this._storage[fromNode]['edges'][toNode];
   delete this._storage[toNode]['edges'][fromNode];
+  if (Object.keys(this._storage[fromNode]['edges']).length === 0) {
+    delete this._storage[fromNode];
+  }
+  if (Object.keys(this._storage[toNode]['edges']).length === 0) {
+    delete this._storage[toNode];
+  }
 };
 
 /*
