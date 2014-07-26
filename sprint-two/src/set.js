a18a9701d98a996e196ad108eafc33324311e52b
David <davidnguyen84@gmail.com>
2014-07-26 11:11:24 -0700

    Completed 'set'

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,35 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  // In case this is a first add, create a new container for this._storage
+  if (this._storage === undefined) {
+    this._storage = {};
+  }
+  // set the new container's property equal to the item
+  this._storage[item] = item;
+
+
 };
 
 setPrototype.contains = function(item){
+  // if the item lookup in this._storage doesn't exist, return true
+
+  if (this._storage[item] !== undefined) {
+    return true;
+  }
+  // otherwise return false
+  return false;
 };
 
 setPrototype.remove = function(item){
+  // if the property called item exists in this._storage, delete it
+  if (this._storage[item] === item) {
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
